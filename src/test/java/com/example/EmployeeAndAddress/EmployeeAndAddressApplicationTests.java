3cebaef2df5a82b853c4487b35c15b2e81451e3d
i-spm <shivspm54@gmail.com>
2022-04-23 00:28:05 +0530

    initial commit

---
+++ src/test/java/com/example/EmployeeAndAddress/EmployeeAndAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeAndAddress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAndAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
